b38a4b634533be361f540eb3ff7481f2d2af231a
arturj9 <arturjardel9@gmail.com>
2025-12-06 23:58:28 -0300

    chore: setup prisma config v7 and app core structure

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ZodValidationPipe } from 'nestjs-zod';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalPipes(new ZodValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Simula.IA API')
+    .setDescription('API de Banco de Questões Inteligente')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+  console.log(`🚀 Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
+  console.log(`📑 Swagger is running on: http://localhost:${process.env.PORT ?? 3000}/docs`);
+}
+bootstrap();
