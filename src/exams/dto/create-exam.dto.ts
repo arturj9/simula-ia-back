b2e29e1bc933c75c4dd4397ed4c2b762a7fa1258
arturj9 <arturjardel9@gmail.com>
2025-12-23 23:02:40 -0300

    fix(exams): corrigir tipos de dados para questionIds e disciplineId no CreateExamDto

--- src/exams/dto/create-exam.dto.ts
+++ src/exams/dto/create-exam.dto.ts
@@ -7,11 +7,11 @@ const createExamSchema = z
     description: z.string().optional(),
     visibility: z.enum(['PUBLIC', 'PRIVATE']).default('PRIVATE'),
 
-    questionIds: z.array(z.string().uuid()).optional(),
+    questionIds: z.array(z.uuid()).optional(),
 
     generateConfig: z
       .object({
-        disciplineId: z.string().uuid(),
+        disciplineId: z.uuid(),
         difficulty: z.enum(['EASY', 'MEDIUM', 'HARD']).optional(),
         count: z.number().min(1).max(50).default(10),
       })
